cd8b320e7dde315571f886025a33d55986e61f22
Olamide Samuel <sam99kupo@gmail.com>
2020-02-04 23:20:31 +0100

    added a few more pages

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,28 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/contact_us', (req, res) => {
+  res.render('contact', {
+    title: 'Contact Us'
+  });
+});
+
 router.get('/lecturers', (req, res) => {
   res.render('lecturer', {
     title: 'Teacher'
   });
 });
 
+router.get('/lecturer-login', (req, res) => {
+  res.render('lectureLogin', {
+    title: "Lecturer's || Login",
+  })
+})
+router.get('/lecturer_sign_up', (req, res) => {
+  res.render('lectureSignup', {
+    title: "Lecturer's || Sign Up",
+  })
+})
 router.get('/student', (req, res) => {
   res.render('student', {
     title: 'Student'
